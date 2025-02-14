960e0fec06c3622b2fa19c18ca0d2fae23bcd4b3
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-02-14 18:01:04 +0000

    Added basic_math.py

---
+++ “basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Aaron Rivelino
+# Date: Feb 15, 2025
+# The code does simple math calculations
+
+print("3+3 = {}".format(3 + 3))
+print("12-4 = {}".format(12 - 4))
+print("{:.2f}".format(3.1415926))
+print("15 x 6 = {}".format(15 * 6))
+print("17/13 = {}".format(17 / 13))
+print("5-4 = {}".format(5 - 4))
+print("5^5 = {}".format(5**5))
+print("5-4 = {}".format(5 - 4))
+import math
+
+sqrt_result = math.sqrt(26)
+print("Square root of 26 =", sqrt_result)
